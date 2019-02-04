faae015c8872d89fbee855f58776b8a47da83bec
Cryosith <caseyvandyke@ymail.com>
2019-02-04 16:29:12 -0700

    final

--- server.js
+++ server.js
@@ -117,24 +117,20 @@ app.get("/api/*", jwtAuth, (req, res) => {
 // this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(
-      databaseUrl,
-      { useNewUrlParser: true },
-      err => {
-        if (err) {
-          return reject(err);
-        }
-        server = app
-          .listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve();
-          })
-          .on("error", err => {
-            mongoose.disconnect();
-            reject(err);
-          });
+    mongoose.connect(databaseUrl, { useNewUrlParser: true }, err => {
+      if (err) {
+        return reject(err);
       }
-    );
+      server = app
+        .listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+        .on("error", err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
   });
 }
 
